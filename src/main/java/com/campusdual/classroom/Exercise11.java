94447cd65224650ce0cd94853ad80cd72b22274c
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-17 15:56:19 +0200

    Exercise11acabado

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,44 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
-    }
 
+        //Encender la TV
+        redRemote.turnOn();
+        if (redRemote.on) {
+            System.out.println("La TV está encendida");
+        } else {
+            System.out.println("La TV está apagada");
+        }
+        //Subir un canal
+        redRemote.channelUp();
+        System.out.println("El canal es: " + redRemote.getChannel());
+
+        //Bajar un canal
+        redRemote.channelDown();
+        System.out.println("El canal es: " + redRemote.getChannel());
+
+        // Establecer el canal en 0, intentar bajar un canal y comprobar que no se puede
+        redRemote.channel = 0;
+        System.out.println("El canal es 0");
+
+        //Subir el volumen
+        redRemote.volumeUp();
+        System.out.println("Volumen = " + redRemote.getVolume());
+
+        //Bajar el volumen
+        redRemote.volumeDown();
+        System.out.println("Volumen = " + redRemote.getVolume());
+
+        //Establecer el volumen en 0, intentar bajar el volumen y comprobar que no se puede
+        redRemote.volume = 0;
+        System.out.println("Volumen = 0");
+
+        //Apagar la TV
+        redRemote.turnOff();
+        if (redRemote.on) {
+            System.out.println("La TV está encendida");
+        }else{
+            System.out.println("La TV está apagada");
+        }
+    }
 }
